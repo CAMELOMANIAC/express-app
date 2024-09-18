c9ae458207140cd34e1683576ca6c488775c2dd8
yugyusang <yugyusang12@gmail.com>
2024-09-19 04:45:55 +0900

    fix: cors 미들웨어 실행위치 변경 및 로깅 미들웨어추가

--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,17 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
+app.use(corsMiddleware); //cors 정책을 적용하는 미들웨어
 app.use(express.json()); //JSON 형식의 요청을 파싱할 수 있도록 설정
 app.use(express.urlencoded({ extended: true })); // URL 인코딩된 형식의 요청을 파싱할 수 있도록 설정
-app.use(corsMiddleware); //cors 정책을 적용하는 미들웨어
 app.use(helloWorldMiddleware); //send 메서드를 재정의하여 응답에 문자열을 추가하는 미들웨어
-app.use("/", router);
+app.use(router);
+
+// 로깅 미들웨어
+app.use((req, _res, next) => {
+  console.log(`Request received: ${req.method} ${req.url}`);
+  next();
+});
 
 app.use((_req, res) => {
   res.status(404).json({ error: "존재하지 않는 요청입니다" });
